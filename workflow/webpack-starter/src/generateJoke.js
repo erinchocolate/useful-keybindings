edf079e5c77328c88f00536b1a600c39bb2ae8cc
Meiqiao <chenmeiqiao940926@gmail.com>
2022-06-08 15:27:14 +1200

    keybindings

--- workflow/webpack-starter/src/generateJoke.js
+++
@@ -1,5 +0,0 @@
-function generateJoke() {
-  return "haha"
-}
-
-export default generateJoke
--- workflow/webpack-starter/src/index.js
+++
@@ -1,4 +0,0 @@
-import gerneateJoke from "./generateJoke";
-import './styles/main.scss'
-
-console.log(gerneateJoke())
